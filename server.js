1262aa820264930a66dfc1571f9b34cc89862f69
nicolas.matet <nicolas.matet@algonomia.com>
2021-10-21 00:25:46 +0200

    fix server

--- server.js
+++ server.js
@@ -6,11 +6,11 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/animal-game'));
+app.use(express.static(__dirname + '/dist/dtp'));
 
 app.get('/*', function(req,res) {
 
-  res.sendFile(path.join(__dirname+'/dist/animal-game/index.html'));
+  res.sendFile(path.join(__dirname+'/dist/dtp/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
